9bf035aff737d7139b4a98280174b20f196dfcdf
arthur <arthur.jourdan@epitech.eu>
2020-04-28 22:59:17 +0200

    feat(set_text.c & set_button.c): disappear delay for texts work & trials on buttons and spritesheets not beautifull

--- src/events/button_management.c
+++ src/events/button_management.c
@@ -29,18 +29,28 @@ static int is_button_pressed(global_t *global)
 
 static void change_button_state(button_t *button, unsigned short state)
 {
-    // if (button->nb_animations) {
-    //     set_texture_rect_shape(button->rect, button->nb_animations,
-    //     button->size_tot);
-    // }
+    sfIntRect rectangle = sfRectangleShape_getTextureRect(button->rect);
+
+    rectangle.left = 0;
     if (state == IDLE) {
         sfRectangleShape_setOutlineColor(button->rect, sfWhite);
+        if (button->nb_animations) {
+            sfRectangleShape_setTextureRect(button->rect, rectangle);
+        }
     }
     if (state == HOVER) {
         sfRectangleShape_setOutlineColor(button->rect, sfBlack);
+        if (button->nb_animations) {
+            set_texture_rect_shape(button->rect, button->nb_animations,
+            button->size_tot);
+        }
     }
     if (state == CLICKED) {
         sfRectangleShape_setOutlineColor(button->rect, sfTransparent);
+        if (button->nb_animations) {
+            set_texture_rect_shape(button->rect, button->nb_animations,
+            button->size_tot);
+        }
     }
 }
 
